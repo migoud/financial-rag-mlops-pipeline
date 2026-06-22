beea9081b7f92460948fe6187c58ad92754983d5
migoud <michaela.c@live.ca>
2026-06-21 18:50:35 -0600

    Modify GDP prediction model input and filter
    Updated the model to use 'gdp_per_capita_local_currency' as the input label and added a filter for country code 'CAN'.

--- sql/2_model_training.sql
+++ sql/2_model_training.sql
@@ -1,13 +1,15 @@
 CREATE OR REPLACE MODEL `project-2e0885aa-8f3e-4da5-86a.finance_ml_engine.gdp_prediction_model`
 OPTIONS(
   model_type='linear_reg',
-  input_label_cols=['gdp_per_capita'],
+  input_label_cols=['gdp_per_capita_local_currency'],
   optimize_strategy='batch_gradient_descent',
   l1_reg=0.1,
   l2_reg=0.1
 ) AS
 SELECT 
   us_cpi,
-  gdp_per_capita
+  gdp_per_capita_local_currency
 FROM 
   `project-2e0885aa-8f3e-4da5-86a.finance_ml_engine.cleaned_features`;
+WHERE
+  country_code = 'CAN' ;
