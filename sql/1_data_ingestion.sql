51b95fde6098546ea020125f4782af045825030d
migoud <michaela.c@live.ca>
2026-06-21 18:49:08 -0600

    Modify data ingestion SQL for CPI and GDP adjustments
    Updated the SQL query to include average US CPI and adjusted filtering for GDP per capita in local currency for Canada and the USA.

--- sql/1_data_ingestion.sql
+++ sql/1_data_ingestion.sql
@@ -1,16 +1,28 @@
 CREATE OR REPLACE TABLE `project-2e0885aa-8f3e-4da5-86a.finance_ml_engine.cleaned_features` AS
+WITH annual_us_cpi AS (
+  SELECT 
+    year,
+    AVG(value) AS avg_us_cpi
+  FROM 
+    `bigquery-public-data.bls.cpi_u`
+  WHERE 
+    series_id = 'CUUR0000SA0'
+    AND year >= 2010
+  GROUP BY 
+    year
+)
 SELECT 
-  bls_data.year,
-  bls_data.value AS us_cpi,
+  cpi.year,
+  ROUND(cpi.avg_us_cpi, 2) AS us_cpi,
+  wdi_data.country_code,
   wdi_data.country_name,
-  wdi_data.value AS gdp_per_capita
+  -- This will be in CAD for Canada, and USD for the United States
+  ROUND(wdi_data.value, 2) AS gdp_per_capita_local_currency 
 FROM 
-  `bigquery-public-data.bls.cpi_u` AS bls_data
+  annual_us_cpi AS cpi
 JOIN 
   `bigquery-public-data.world_bank_wdi.indicators_data` AS wdi_data
-  ON bls_data.year = wdi_data.year
+  ON cpi.year = wdi_data.year
 WHERE 
-  bls_data.series_id = 'CUUR0000SA0'
-  AND wdi_data.indicator_name = 'GDP per capita (current US$)'
-  AND wdi_data.country_code = 'CAN'
-  AND bls_data.year >= 2010;
+  wdi_data.indicator_name = 'GDP per capita (current LCU)' -- Local Currency Unit (CAD / USD)
+  AND wdi_data.country_code IN ('CAN', 'USA'); -- Filtered strictly to Canada and the United States
